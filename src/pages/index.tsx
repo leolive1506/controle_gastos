6c1489bf03acf085afa23f714ec65313c7f66d2c
Leonardo Lop <leonardolivelopes2@gmail.com>
2022-04-09 12:26:15 -0300

    install axios and react hook and yup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Página de login</h1>
+    </div>
+  )
+}
+
+export default Home
